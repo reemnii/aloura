e0212aafd09ed8025c21386a1499c36f1eb25960
Reem Nemr <rnimer9@gmail.com>
2026-06-09 17:07:13 +0300

    Update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import Navbar from "../src/components/Navbar.jsx";
-import Hero from "../src/components/Hero.jsx";
-import Banner from "../src/components/Banner";
+import Navbar from "../src/Components/Navbar.jsx";
+import Hero from "../src/Components/Hero.jsx";
+import Banner from "../src/Components/Banner.jsx";
 
 export default function Page() {
   const [bannerVisible, setBannerVisible] = useState(true);
